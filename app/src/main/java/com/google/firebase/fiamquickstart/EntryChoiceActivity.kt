836b0ef7980a740bcc7814c0cfca033a1a20d3e8
Ryan Cheung <cheungcmtmp@gmail.com>
2020-01-26 14:33:50 +0800

    Trigger event on landing activity (i.e. EntryChoiceActivity)

--- app/src/main/java/com/google/firebase/fiamquickstart/EntryChoiceActivity.kt
+++ app/src/main/java/com/google/firebase/fiamquickstart/EntryChoiceActivity.kt
@@ -1,12 +1,33 @@
 package com.google.firebase.fiamquickstart
 
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import com.firebase.example.internal.BaseEntryChoiceActivity
 import com.firebase.example.internal.Choice
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.fiamquickstart.java.MainActivity
 import com.google.firebase.fiamquickstart.kotlin.KotlinMainActivity
+import com.google.firebase.inappmessaging.FirebaseInAppMessaging
 
 class EntryChoiceActivity : BaseEntryChoiceActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    private lateinit var firebaseIam: FirebaseInAppMessaging
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        firebaseIam = FirebaseInAppMessaging.getInstance()
+
+        firebaseIam.isAutomaticDataCollectionEnabled = true
+        firebaseIam.setMessagesSuppressed(false)
+
+        FirebaseInAppMessaging.getInstance().triggerEvent("view_entry_choice")
+
+        Log.d("EntryChoiceActivity", "event: view_entry_choice")
+    }
 
     override fun getChoices(): List<Choice> {
         return listOf(
